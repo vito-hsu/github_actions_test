d3382c6e3121b6df75e4480ee11bbcf717a12746
vito-hsu <garraypierce@gmail.com>
2023-05-04 10:05:37 +0800

    first version

---
+++ 20230504.py
@@ -0,0 +1,23 @@
+# Step1. PULL .yml file from GitHub
+#   git init
+#   git remote add origin https://github.com/vito-hsu/github_actions_test.git
+#   git pull origin main
+
+# Step2. PUSH .py file to GitHub
+#   git add .
+#   git commit -m 'first version'
+#   git push origin main
+
+
+def test_export_report_1():
+    a = 1 + 1
+    b = 2 + 2
+
+    assert b > a
+
+
+def test_export_report_2():
+    a = 2 + 2
+    b = 4 + 4
+
+    assert a < b
