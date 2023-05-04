d241bfc4c766761aefe1a372e34704eed43dec90
vito-hsu <garraypierce@gmail.com>
2023-05-04 10:07:35 +0800

    version_2

--- 20230504.py
+++ 20230504.py
@@ -9,15 +9,13 @@
 #   git push origin main
 
 
-def test_export_report_1():
+def test_1():
     a = 1 + 1
     b = 2 + 2
-
     assert b > a
 
 
-def test_export_report_2():
+def test_2():
     a = 2 + 2
     b = 4 + 4
-
     assert a < b
