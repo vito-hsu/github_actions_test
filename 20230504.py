035cc7e13476562d9c5389532b947a572175b0b1
vito-hsu <garraypierce@gmail.com>
2023-05-04 10:12:01 +0800

    version_4

--- 20230504.py
+++ 20230504.py
@@ -1,9 +1,9 @@
-# Step1. PULL .yml file from GitHub
+# Step1. PULL files from GitHub
 #   git init
 #   git remote add origin https://github.com/vito-hsu/github_actions_test.git
 #   git pull origin main
 
-# Step2. PUSH .py file to GitHub
+# Step2. PUSH (updated) files to GitHub
 #   git add .
 #   git commit -m 'first version'
 #   git push origin main
@@ -20,3 +20,8 @@ def test_2():
     b = 4 + 4
     assert a < b
 
+
+def test_3():
+    a = 2 + 2
+    b = 4 + 4
+    assert a == b
